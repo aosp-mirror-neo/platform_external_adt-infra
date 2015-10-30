539a2bce47d75979e4e0e28d26264dcea66de802
Ying Chen <chying@google.com>
2015-10-30 11:04:44 -0700

    Update recipe adt.py to work on different platforms, add CTS test step
    - find android sdk home based on platform architecture
    - on windows buidler, add cygwin and gunwin bin directory to path
    - find emulator script by location of recipe directory
    - add CTS test step
    
    Change-Id: Ibca0be6a27d3ea17633638e8534fe79ac519be2e

--- build/scripts/slave/recipes/adt/adt.py
+++ build/scripts/slave/recipes/adt/adt.py
@@ -2,9 +2,11 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-"""Launches emulator CTS tests."""
+"""Recipe for emulator boot tests."""
 
 from recipe_engine.types import freeze
+import os
+import platform
 
 DEPS = [
     'path',
@@ -25,20 +27,60 @@ def RunSteps(api):
   download_path = api.path['slave_build'].join('')
   remote_path = '%s@%s:%s' % (MASTER_USER, MASTER_IP, image_file_path)
 
-  api.step('Clean slave build directory', ['rm', '-rf', download_path.join('tools')])
-  #api.step('Clean slave build directory', ['rm', '-rf', download_path.join('*')])
-  api.step('Download Image', ['scp', remote_path, download_path])
   local_zipfile = download_path.join(image_filename)
-  api.step('Unzip Image File', ['unzip', local_zipfile])
-
-  dotest_path = '/home/adt_build/Buildbot/emu_scripts/dotest.py'
   emulator_path = download_path.join('tools', 'emulator')
-  android_path = '/home/adt_build/Android/android-sdk-linux/tools/'
-  android_platform_path = '/home/adt_build/Android/android-sdk-linux/platform-tools/'
-  env = {'PATH': api.path.pathsep.join([android_path, android_platform_path, '%(PATH)s'])}
-  api.python('Run Emulator Tests', dotest_path,
-             ['-l', 'INFO', '-exec', emulator_path], env=env)
 
+  env_path = ['%(PATH)s']
+
+  # find android sdk root directory
+  home_dir = os.path.expanduser('~')
+  host = platform.system()
+  if host == "Darwin":
+    android_sdk_home = os.path.join(home_dir, 'Android/android-sdk-macosx')
+  elif host == "Linux":
+    android_sdk_home = os.path.join(home_dir, 'Android/android-sdk-linux')
+  # On windows, we need cygwin and GnuWin for commands like, rm, scp, unzip
+  elif host == "Windows":
+    if 'x86' in os.environ['PROGRAMFILES']:
+      android_sdk_home = 'C:\\Program Files (x86)\\Android\\android-sdk'
+      gnu_path = 'C:\\Program Files (x86)\\GnuWin32\\bin'
+      cygwin_path = 'C:\\cygwin64\\bin'
+    else:
+      android_sdk_home = 'C:\\Program Files\\Android\\android-sdk'
+      gnu_path = 'C:\\Program Files\\GnuWin32\\bin'
+      cygwin_path = 'C:\\cygwin\\bin'
+    env_path += [gnu_path, cygwin_path]
+  else:
+    raise
+
+  android_tools_dir = os.path.join(android_sdk_home, 'tools')
+  android_platform_dir = os.path.join(android_sdk_home, 'platform-tools')
+  env_path += [android_tools_dir, android_platform_dir]
+  env = {'PATH': api.path.pathsep.join(env_path),
+         'ANDROID_SDK_ROOT': android_sdk_home}
+
+  # Find emulator script based on current location
+  # Current directory should be [project root]/build/scripts/slave/recipes/[recipeName]/
+  # Emulator scripts are located [project root]/emu_test
+  recipe_dir = os.path.dirname(os.path.realpath(__file__))
+  dotest_path = os.path.join(recipe_dir, '..', '..', '..', '..', '..', 'emu_test', 'dotest.py')
+
+  api.step('Clean slave build directory',
+           ['rm', '-rf', download_path.join('tools')],
+           env=env)
+  api.step('Download Image',
+           ['scp', remote_path,
+           '.'],
+           env=env)
+  api.step('Unzip Image File',
+           ['unzip', local_zipfile],
+           env=env)
+  api.python('Run Emulator Boot Test', dotest_path,
+             ['-l', 'DEBUG', '-exec', emulator_path, '-p', 'test_boot.*'],
+             env=env)
+  api.python('Run Emulator CTS Test', dotest_path,
+             ['-l', 'DEBUG', '-exec', emulator_path, '-p', 'test_cts.*'],
+             env=env)
 
 def GenTests(api):
   yield (
